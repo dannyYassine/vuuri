48c244fefa67ccd59fa93b701c0e98f3cab9ce6c
Danny <dannyyassine@gmail.com>
2020-12-27 17:56:53 -0500

    no message

--- .docs/demo/src/App.vue
+++ .docs/demo/src/App.vue
@@ -9,12 +9,11 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 </style>
