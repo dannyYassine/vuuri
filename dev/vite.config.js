3e5fbd0254c1d02f9f63d82b6ec61ef48ef549da
Danny Yassine <dannyyassine@gmail.com>
2024-02-25 20:56:24 +0000

    added more

--- dev/vite.config.js
+++ dev/vite.config.js
@@ -6,14 +6,13 @@ import istanbul from 'vite-plugin-istanbul';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), istanbul({
-    include: '@vuuri/**/*',
+    include: './../src/**/*',
     exclude: ['node_modules', 'tests/'],
     extension: [ '.js', '.ts', '.vue' ],
-    requireEnv: false,
-    checkProd: false,
+    requireEnv: false
   }),],
   build: {
-    sourcemap: 'inline'
+    sourcemap: true
   },
   server: {
     host: true,
