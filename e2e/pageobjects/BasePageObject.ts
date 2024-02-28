5877b1f6fd19c9db9cbd8991607225ffc9a6f7f9
Danny Yassine <dannyyassine@gmail.com>
2024-02-28 01:49:58 +0000

    able to to use pw for crud operations

--- e2e/pageobjects/BasePageObject.ts
+++ e2e/pageobjects/BasePageObject.ts
@@ -1,35 +1,30 @@
 import { expect, Locator, Page } from '@playwright/test';
 
 export class BasePageObject {
+  constructor(protected page: Page) {}
 
-    constructor(protected page: Page) {
-
-    }
-
-    getPort() {
-      return '9999'
-    }
-    
-    getHost() {
-      return 'http://localhost'
-    }
-    
-    getUrl() {
-      return `${this.getHost()}:${this.getPort()}/#/e2e/${this.getPath()}`
-    }
-    
-    /**
-     * @abstract
-     */
-    getPath() {
-    
-    }
-
-    goTo(appPath: string): Promise<null | Response> {
-      return this.page.goto(`${process.env.CLIENT_URL}/${appPath}`);
-    }
-    
-    visit() {
-      cy.visit(`${process.env('PW_CLIENT_URL') || this.getUrl()}`);
-    }
+  getPort() {
+    return '3000';
   }
+
+  getHost() {
+    return 'http://client';
+  }
+
+  getUrl() {
+    return `${this.getHost()}:${this.getPort()}/#/e2e/${this.getPath()}`;
+  }
+
+  /**
+   * @abstract
+   */
+  getPath() {}
+
+  // goTo(appPath: string): Promise<null | Response> {
+  //   return this.page.goto(`${process.env.CLIENT_URL}/${appPath}`);
+  // }
+
+  visit() {
+    this.page.goto(`${this.getUrl()}`);
+  }
+}
--- e2e/specs/crud.spec.ts
+++ e2e/specs/crud.spec.ts
@@ -1,47 +1,46 @@
+import { expect, test } from 'playwright-test-coverage';
 import { CrudPageObject } from '../pageobjects/CrudPageObject';
 import { wait } from '../utils';
-import { test, expect } from "playwright-test-coverage";
 
-test.describe.skip('CRUD ', () => {
-    test('should add new items', async ({page}) => {
+test.describe('CRUD ', () => {
+  test('should add new items', async ({ page }) => {
     const crudPageObject = new CrudPageObject(page);
     crudPageObject.visit();
-    
+
     await wait(2000);
-  
-    expect(crudPageObject.getItems().count().length).toEqual(1);
-  
+
+    let initialCount = await crudPageObject.getItems().count();
+
     await page.evaluate(async () => {
       const component = window.__components__.AddDeleteDemo;
       component.items.push({
         id: Math.random(),
-        name: Math.random()+''
+        name: Math.random() + ''
       });
     });
-    
+
     await wait(2000);
-  
-    expect(crudPageObject.getItems().count().length).toEqual(2);
+
+    const newCount = await crudPageObject.getItems().count();
+    expect(newCount).toEqual(++initialCount);
   });
-  
-  test('should delete items', async ({page}) => {
+
+  test('should delete items', async ({ page }) => {
     const crudPageObject = new CrudPageObject(page);
     crudPageObject.visit();
-  
+
     await wait(2000);
-  
-    expect(crudPageObject.getItems().count().length).toEqual(0);
-  
+
+    let initialCount = await crudPageObject.getItems().count();
+
     await page.evaluate(async () => {
-        const component = window.__components__.AddDeleteDemo;
-        component.items.push({
-          id: Math.random(),
-          name: Math.random()+''
-        });
+      const component = window.__components__.AddDeleteDemo;
+      component.items.splice(0, 1);
     });
-    
+
     await wait(2000);
-    
-    expect(crudPageObject.getItems().count().length).toEqual(0);
+
+    const newCount = await crudPageObject.getItems().count();
+    expect(newCount).toEqual(--initialCount);
   });
-})
+});
