1fbfa0607d93f9caa2c66a155fb33db976972f0b
Danny Yassine <dannyyassine@gmail.com>
2024-02-29 13:33:28 +0000

    added waits

--- e2e/specs/dragenabled.spec.ts
+++ e2e/specs/dragenabled.spec.ts
@@ -1,18 +1,25 @@
 import { expect, test } from 'playwright-test-coverage';
 import { DragEnabledPageObject } from '../pageobjects/DragEnabledPageObject';
+import { wait } from '../utils';
 
 test.describe('CRUD ', () => {
   test('should drag and drop item in same grid left to right', async ({ page }) => {
     const dragEnabledPageObject = new DragEnabledPageObject(page);
     dragEnabledPageObject.visit();
 
+    await wait(1000);
+
     const firstItem = dragEnabledPageObject.getItemAtIndex(0);
     const secondItem = dragEnabledPageObject.getItemAtIndex(1);
 
     await firstItem.hover();
+    await wait(500);
     await page.mouse.down();
+    await wait(500);
     await secondItem.hover();
+    await wait(500);
     await page.mouse.up();
+    await wait(500);
 
     const firstBox = await firstItem.boundingBox();
     const secondBox = await secondItem.boundingBox();
@@ -24,13 +31,19 @@ test.describe('CRUD ', () => {
     const dragEnabledPageObject = new DragEnabledPageObject(page);
     dragEnabledPageObject.visit();
 
+    await wait(1000);
+
     const firstItem = dragEnabledPageObject.getItemAtIndex(0);
     const secondItem = dragEnabledPageObject.getItemAtIndex(1);
 
     await secondItem.hover();
+    await wait(500);
     await page.mouse.down();
+    await wait(500);
     await firstItem.hover();
+    await wait(500);
     await page.mouse.up();
+    await wait(500);
 
     const firstBox = await firstItem.boundingBox();
     const secondBox = await secondItem.boundingBox();
