17f87002d6276541cb1724f51558290ac7e0b0bf
Danny Yassine <dannyyassine@gmail.com>
2024-02-29 09:58:42 -0500

    remove unwanted code

--- e2e/pageobjects/GroupDragEnabledPageObject.ts
+++ e2e/pageobjects/GroupDragEnabledPageObject.ts
@@ -1,19 +1,19 @@
-import { BasePageObject } from './BasePageObject';
+import { BasePageObject } from "./BasePageObject";
 
 export class GroupDragEnabledPageObject extends BasePageObject {
   getPath() {
-    return 'group-drag-enabled';
-  }
-
-  getGridAtIndex(index = 0) {
-    return this.page.locator('.muuri-grid').nth(index);
+    return "group-drag-enabled";
   }
 
   getItemsAtIndex(index = 0) {
-    return this.page.locator('.muuri-grid').nth(index).locator('.muuri-item');
+    return this.page.locator(".muuri-grid").nth(index).locator(".muuri-item");
   }
 
   getItemAtGridIndexForIndex(gridIndex = 0, index = 0) {
-    return this.page.locator('.muuri-grid').nth(gridIndex).locator('.muuri-item').nth(index);
+    return this.page
+      .locator(".muuri-grid")
+      .nth(gridIndex)
+      .locator(".muuri-item")
+      .nth(index);
   }
 }
