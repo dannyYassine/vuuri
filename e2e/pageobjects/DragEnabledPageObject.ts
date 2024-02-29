12f15242c22ef69a6f0556536a73b5a4a2a69566
Danny Yassine <dannyyassine@gmail.com>
2024-02-29 14:39:34 +0000

    remove unused code

--- e2e/pageobjects/DragEnabledPageObject.ts
+++ e2e/pageobjects/DragEnabledPageObject.ts
@@ -1,16 +1,8 @@
-import { BasePageObject } from "./BasePageObject";
+import { BasePageObject } from './BasePageObject';
 
 export class DragEnabledPageObject extends BasePageObject {
   getPath() {
     return 'drag-enabled';
-  }
-
-  getGrid() {
-    return this.page.locator(".muuri-grid");
-  }
-
-  getItems() {
-    return this.page.locator(".muuri-grid .muuri-item");
   }
 
   getItemAtIndex(index = 0) {
